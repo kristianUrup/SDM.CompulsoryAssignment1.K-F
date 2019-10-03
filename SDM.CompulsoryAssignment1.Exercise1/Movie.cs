0a15f94e9732d5a32d05e850329644df500bbee3
kristianUrup <43743914+kristianUrup@users.noreply.github.com>
2019-10-03 09:13:09 +0200

    prøv igen FREDDY

--- SDM.CompulsoryAssignment1.Exercise1/Movie.cs
+++ SDM.CompulsoryAssignment1.Exercise1/Movie.cs
@@ -6,8 +6,8 @@ namespace SDM.CompulsoryAssignment1.Exercise1
 {
     public class Movie
     {
+        public int Id { get; set; }
         public int Reviewer { get; set; }
-        public int Movie { get; set; }
         public int Grade { get; set; }
         public DateTime Date { get; set; }
     }
--- SDM.CompulsoryAssignment1.Exercise1/MovieSystem.cs
+++ SDM.CompulsoryAssignment1.Exercise1/MovieSystem.cs
@@ -17,6 +17,7 @@ namespace SDM.CompulsoryAssignment1.Exercise1
         {
             _movies = movies;
         }
+        //Exercise 1
         public int ReviewsById(int reviewerId)
         {
             int numOfReview = 0;
@@ -35,12 +36,94 @@ namespace SDM.CompulsoryAssignment1.Exercise1
                 return numOfReview;
             } 
         }
-
+        //Exercise 2
         public double AverageScoreById(int reviewerId)
+        {
+            double sumRating = 0;
+            double amountOfRatings = 0;
+            foreach (var movie in _movies)
+            {
+                if (reviewerId == movie.Reviewer)
+                {
+                    sumRating = sumRating + movie.Grade;
+                    amountOfRatings++;
+                }
+            }
+
+            if (sumRating <= 0)
+            {
+                throw new InvalidDataException("Id not found");
+            }
+            return sumRating / amountOfRatings;
+
+        }
+
+        //Exercise 3
+        public int AmountOfGradesById(int reviewerId)
+        {
+            int gradesById = 0;
+            foreach (var movie in _movies)
+            {
+                if (movie.Reviewer == reviewerId)
+                {
+                    gradesById++;
+                }
+            }
+
+            if (gradesById == 0)
+            {
+                throw new InvalidDataException("The id was not found");
+            }
+
+            return gradesById;
+        }
+
+        //Exercise 4
+        public int ReviewedMovie(int movieId)
+        {
+            throw new NotImplementedException();
+        }
+        
+        //Exercise 5
+        public double RateOfMovie(int movieId)
         {
             throw new NotImplementedException();
         }
 
+        //Exercise 6
+        public int MovieRecievedSpecificRate(int movieId, int Rate)
+        {
+            throw new NotImplementedException();
+        }
 
+        //Exercise 7
+        public int MoviesWithTopRate5()
+        {
+            throw new NotImplementedException();
+        }
+
+        //Exercise 8 
+        public int ReviewerWatchedMostMovies()
+        {
+            throw new NotImplementedException();
+        }
+
+        //Exercise 9
+        public int TopXMovies(int numberOfMovies)
+        {
+            throw new NotImplementedException();
+        }
+
+        //Exercise 10
+        public List<Movie> SortedRateAndDateById()
+        {
+            throw new NotImplementedException();
+        }
+
+        //Exercise 11
+        public List<Movie> SortedReviewerReviewedMovieById()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
