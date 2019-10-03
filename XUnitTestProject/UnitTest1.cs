cc1e93e8d3ff1ac1979aec51e4d520416a44825f
Frederik Jensen <kungfu15447@users.noreply.github.com>
2019-10-03 09:30:20 +0200

    Tilføjet test klasse

--- XUnitTestProject/UnitTest1.cs
+++ XUnitTestProject/UnitTest1.cs
@@ -1,4 +1,6 @@
+using SDM.CompulsoryAssignment1.Exercise1;
 using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace XUnitTestProject
@@ -6,9 +8,10 @@ namespace XUnitTestProject
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void TestReviewsById()
         {
-
+            List<Movie> movies = new List<Movie>();
+            MovieSystem ms = new MovieSystem(movies);
         }
     }
 }
