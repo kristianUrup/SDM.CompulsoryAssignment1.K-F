a5b7085b48df6f41c6b5f67cf51d91672626880e
kristianUrup <43743914+kristianUrup@users.noreply.github.com>
2019-10-03 09:53:48 +0200

    TestAverageScore

--- XUnitTestProject/UnitTest1.cs
+++ XUnitTestProject/UnitTest1.cs
@@ -1,6 +1,7 @@
 using SDM.CompulsoryAssignment1.Exercise1;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
 using Xunit;
 
@@ -41,6 +42,24 @@ namespace XUnitTestProject
             };
             MovieSystem ms = new MovieSystem(movies);
             Assert.Throws<InvalidDataException>(() => ms.ReviewsById(reviewer));
+        }
+
+        [Theory]
+        [InlineData(1, 1)]
+        [InlineData(2.5, 2)]
+        [InlineData(4, 3)]
+        public void TestAverageScoreById(double expAverage, int reviewerId)
+        {
+            List<Movie> movies = new List<Movie>()
+            {
+                new Movie { Reviewer = 1,Grade = 1},
+                new Movie { Reviewer = 1, Grade = 1},
+                new Movie { Reviewer = 2, Grade = 2},
+                new Movie { Reviewer = 2,Grade = 3},
+                new Movie { Reviewer = 3,Grade = 4}
+            };
+            MovieSystem ms = new MovieSystem(movies);
+            Assert.Equal(expAverage,ms.AverageScoreById(reviewerId));
         }
     }
 }
