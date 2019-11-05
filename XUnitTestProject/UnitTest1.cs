caec81aa6df9e8b3b7f784615a5a7ee245f85fa9
kristianUrup <43743914+kristianUrup@users.noreply.github.com>
2019-11-05 11:45:03 +0100

    new test

--- XUnitTestProject/UnitTest1.cs
+++ XUnitTestProject/UnitTest1.cs
@@ -155,17 +155,17 @@ namespace XUnitTestProject
         
         [Theory]
         [InlineData(1,1)]
-        [InlineData(2.5, 2)]
-        [InlineData(2.5, 3)]
+        [InlineData(2, 2)]
+        [InlineData(3, 3)]
         public void TestAverageGradeOfMovie(double expRate, int movieId)
         {
             List<Movie> movies = new List<Movie>()
             {
                 new Movie { Reviewer = 1, Grade = 1,MovieId = 1},
-                new Movie { Reviewer = 1, Grade = 1,MovieId = 2},
-                new Movie { Reviewer = 2, Grade = 2, MovieId = 3},
+                new Movie { Reviewer = 1, Grade = 2,MovieId = 2},
                 new Movie { Reviewer = 2, Grade = 3, MovieId = 3},
-                new Movie { Reviewer = 3, Grade = 4, MovieId = 2}
+                new Movie { Reviewer = 2, Grade = 3, MovieId = 3},
+                new Movie { Reviewer = 3, Grade = 2, MovieId = 2}
             };
             MovieSystem ms = new MovieSystem(movies);
             Assert.Equal(expRate,ms.AverageGradeOfMovie(movieId));
